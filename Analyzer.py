9ef8972d8c93a4ccadf934762bf4ed72b1002979
19gaw <19itgawri97@gmail.com>
2023-09-06 12:06:29 +0200

    Ósmy Projekt (EN/PL)

--- Analyzer.py
+++ Analyzer.py
@@ -1,8 +1,8 @@
 def check_odd_even(number):
   if number%2 == 0:
-    print(f'{number} is an even number')
+    return (f'{number} is an even number')
   else:
-    print(f'{number} is an odd number')
+    return (f'{number} is an odd number')
 
 def get_factors(number):
   factors = []
@@ -16,9 +16,9 @@ def get_factors(number):
 
 def check_prime(number, factors):
   if len(factors) == 2:
-    print(f'{number} is a prime number')
+    return (f'{number} is a prime number')
   else:
-    print(f'{number} is not a prime number')
+    return (f'{number} is not a prime number')
 
 def analyze_number(number):
   print(check_odd_even(number))
@@ -26,3 +26,35 @@ def analyze_number(number):
   print(check_prime(number, factors))
 
 analyze_number(15)
+
+# PL----------------------------------------------------------------
+
+def sprawdz_parzysta_nieparzysta(liczba):
+  if liczba % 2 == 0:
+    return(f'{liczba} jest liczbą parzystą')
+  else:
+    return(f'{liczba} jest liczbą nieparzystą')
+
+def znajdz_dzielniki(liczba):
+  dzielniki = []
+  indeks_iteracji = 1
+  while indeks_iteracji <= liczba:
+    if liczba % indeks_iteracji == 0:
+      dzielniki.append(indeks_iteracji)
+    indeks_iteracji += 1
+  print(f'Dzielniki liczby {liczba} to: {dzielniki}')
+  return dzielniki
+
+def sprawdz_pierwsza(liczba, dzielniki):
+  if len(dzielniki) == 2:
+    return(f'{liczba} jest liczbą pierwszą')
+  else:
+    return(f'{liczba} nie jest liczbą pierwszą')
+
+def analizuj_liczbe(liczba):
+  print(sprawdz_parzysta_nieparzysta(liczba))
+  dzielniki = znajdz_dzielniki(liczba)
+  print(sprawdz_pierwsza(liczba, dzielniki))
+
+analizuj_liczbe(15)
+
