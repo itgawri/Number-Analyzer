3e181534c57df83f843c7442c5bee9c6f76f5b99
itgawri <136715144+itgawri@users.noreply.github.com>
2024-01-24 13:20:46 +0100

    Analyzer.py

--- Analyzer.py
+++ Analyzer.py
@@ -26,35 +26,3 @@ def analyze_number(number):
   print(check_prime(number, factors))
 
 analyze_number(15)
-
-# PL----------------------------------------------------------------
-
-def sprawdz_parzysta_nieparzysta(liczba):
-  if liczba % 2 == 0:
-    return(f'{liczba} jest liczbą parzystą')
-  else:
-    return(f'{liczba} jest liczbą nieparzystą')
-
-def znajdz_dzielniki(liczba):
-  dzielniki = []
-  indeks_iteracji = 1
-  while indeks_iteracji <= liczba:
-    if liczba % indeks_iteracji == 0:
-      dzielniki.append(indeks_iteracji)
-    indeks_iteracji += 1
-  print(f'Dzielniki liczby {liczba} to: {dzielniki}')
-  return dzielniki
-
-def sprawdz_pierwsza(liczba, dzielniki):
-  if len(dzielniki) == 2:
-    return(f'{liczba} jest liczbą pierwszą')
-  else:
-    return(f'{liczba} nie jest liczbą pierwszą')
-
-def analizuj_liczbe(liczba):
-  print(sprawdz_parzysta_nieparzysta(liczba))
-  dzielniki = znajdz_dzielniki(liczba)
-  print(sprawdz_pierwsza(liczba, dzielniki))
-
-analizuj_liczbe(15)
-
